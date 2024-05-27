c30b5111ccdb5cd4e0e915ef0486de0075dbcd94
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-05-27 19:46:56 -0400

    Added function that provided publishing new posts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,33 @@
 const submitBtn = document.querySelector('#submit');//const with querySelector that finds the button on index.html
 const titleInput = document.querySelector('#title');//const with querySelector that grabs the value of users input
+const postsEl = document.querySelector('#posts');/*const with querySelector that provides renewal of content 
+in appropriate section of blog.html without changing the file itself*/
 
-
+/*This function below provides obtaining data (posts as a string) from local Storage, 
+the turning those data from string to object,
+then after new value from users input has been gotten turns those data to string
+and sends in to the local starage.
+!This chunk of code was borrowed from Anthony Cooper(Instructor)*/
+const renderPosts = function() {
+    // Retrives data from localStorage if it exists.
+    const data = localStorage.getItem('posts');
+  
+    // Parse the data and tf no posts are stored, provide an empty array
+    const posts = JSON.parse(data) || [];
+  /* The for loop below provides:  */
+    for (let post of posts) {
+      const headerEl = document.createElement('header');
+      const blogPostEl = document.createElement('article');
+      const pEl = document.createElement('p');
+  
+      headerEl.textContent = post.title; 
+      pEl.textContent = "some content to be added later";
+  
+      blogPostEl.appendChild(headerEl);
+      blogPostEl.appendChild(pEl);
+      postsEl.appendChild(blogPostEl);
+    }
+  }
 
 /*This function below provides obtaining data from local Storage, the turning those data from string to object,
 then after new value from users input has been gotten turns those data to string and sends in to the local starage.
@@ -37,5 +63,7 @@ const handleSubmit = function(event) {
   }, 250);
 };
 
-if (submitBtn) submitBtn.addEventListener('click', handleSubmit);
+if (submitBtn) submitBtn.addEventListener('click', handleSubmit);/*Event listiner that starts the "handleSubmit" function after 
+event "click" happens with  hmtl element that signed by the querySelector "submit"*/
+if (postsEl) renderPosts();/*function "renderPosts" launched*/
 
